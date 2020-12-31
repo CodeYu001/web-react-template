e0f4efb374555f45bf08dca5fc1413746300b9f4
杨林 <494657028@qq.com>
2020-12-31 18:10:49 +0800

    first commit

---
+++ config/config.ts
@@ -0,0 +1,59 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+import defaultSettings from './defaultSettings';
+import proxy from './proxy';
+import routes from './routes';
+
+const { REACT_APP_ENV } = process.env;
+
+export default defineConfig({
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  layout: {
+    name: 'Ant Design Pro',
+    locale: false,
+    siderWidth: 208,
+    ...defaultSettings,
+  },
+  locale: {},
+  dynamicImport: {
+    loading: '@ant-design/pro-layout/es/PageLoading',
+  },
+  targets: {
+    ie: 11,
+  },
+  // umi routes: https://umijs.org/docs/routing
+  routes,
+  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
+  theme: {
+    'primary-color': defaultSettings.primaryColor,
+    'font-size-sm': '12px',
+    'text-color': '#333',
+    'layout-body-background': '#f0f2f5',
+    'layout-header-background': '#001529',
+    'tab-left-menu': '#5E5E5E',
+    'black': '#000',
+    'close-icon-hover': '#ccc',
+  },
+  esbuild: {},
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+  // https://github.com/zthxxx/react-dev-inspector
+  plugins: ['react-dev-inspector/plugins/umi/react-inspector'],
+  inspectorConfig: {
+    // loader options type and docs see below
+    exclude: [],
+    babelPlugins: [],
+    babelOptions: {},
+  },
+  resolve: {
+    includes: ['src/components'],
+  },
+});
---
+++ config/routes.ts
@@ -0,0 +1,121 @@
+﻿/**
+ * 注意:
+ * 1、wrappers: ['@/components/KeepAlive'],必须添加
+ * 2、keppAlive 是否缓存当前页面
+ * 3、keepAliveName 为国际化请保证以menu开头拼接
+ * 4、saveScrollPosition 是否缓存滚动条位置，详情：https://github.com/CJY0208/react-activation/blob/master/README_CN.md
+ */
+export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: false,
+    keepAliveName: '欢迎'
+  },
+  {
+    path: '/welcome1',
+    name: 'welcome1',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: true,
+    keepAliveName: '欢迎1'
+  },
+  {
+    path: '/welcome11',
+    name: 'welcome11',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: true,
+    keepAliveName: '欢迎111'
+  },
+  {
+    path: '/welcome2',
+    name: 'welcome2',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: true,
+    keepAliveName: '欢迎12'
+  },
+  {
+    path: '/welcome3',
+    name: 'welcome3',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: true,
+    keepAliveName: '欢迎3'
+  },
+  {
+    path: '/welcome4',
+    name: 'welcome4',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: true,
+    keepAliveName: '欢迎14'
+  },
+  {
+    path: '/welcome5',
+    name: 'welcome5',
+    icon: 'smile',
+    component: './Welcome',
+    wrappers: ['@/components/KeepAlive'],
+    keppAlive: true,
+    keepAliveName: '欢迎15'
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+        wrappers: ['@/components/KeepAlive'],
+        keppAlive: true,
+        keepAliveName: '子页面'
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+    keppAlive: true,
+    wrappers: ['@/components/KeepAlive'],
+    keepAliveName: '列表页'
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
